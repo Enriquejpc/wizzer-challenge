14dd4bcc7ee7a7cfbe9602d65e06e51fd6b15a71
enriquepc <enrique.parada@bonial.com>
2024-01-01 08:27:10 -0300

    first commit

---
+++ src/main/java/com/wizzer/challenge/WizzerChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.wizzer.challenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WizzerChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WizzerChallengeApplication.class, args);
+	}
+
+}
